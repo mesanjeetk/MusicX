d3a97285c50854854306a615f26389b2482b97c8
mesanjeetk <mesanjeetk1432@gmail.com>
2025-07-09 09:35:11 +0530

    commit 4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.mesanjeetk.musicx"
+    namespace = "com.example.musicx"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -20,7 +20,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.mesanjeetk.musicx"
+        applicationId = "com.example.musicx"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
