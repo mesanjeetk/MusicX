c735290cf5d4a792a03c1acc9760cafc65e5d875
mesanjeetk <mesanjeetk1432@gmail.com>
2025-07-11 21:02:18 +0530

    e

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
-
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
+import 'package:provider/provider.dart';
+import 'services/playback_manager.dart';
 import 'screens/music_scanner.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => PlaybackManager(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MusicScanner(),
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      title: 'Music App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MusicScanner(),
     );
   }
 }
